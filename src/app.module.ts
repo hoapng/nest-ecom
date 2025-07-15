db53743f3fcde382884b4c379a23aee2ba7eadf6
hoapng <giahoa01giathieu@gmail.com>
2025-07-15 15:53:25 +0700

    Cache clientRoleId khi đăng ký user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SharedModule } from 'src/shared/shared.module'
+import { AuthModule } from 'src/routes/auth/auth.module'
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
