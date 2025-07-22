bec3d6d83f0ad096f98ef4898fc37d65b1107acb
hoapng <giahoa01giathieu@gmail.com>
2025-07-22 22:41:01 +0700

    Fix bug khi Role bị vô hiệu hóa thì nên từ chối request

--- src/shared/guards/access-token.guard.ts
+++ src/shared/guards/access-token.guard.ts
@@ -51,6 +51,7 @@ export class AccessTokenGuard implements CanActivate {
         where: {
           id: roleId,
           deletedAt: null,
+          isActive: true,
         },
         include: {
           permissions: {
