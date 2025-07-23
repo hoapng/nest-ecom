93d84ff74480dffa7520bbf5ebe62efc5309f658
hoapng <giahoa01giathieu@gmail.com>
2025-07-23 22:13:40 +0700

    Upload single file

---
+++ src/routes/media/media.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common'
+import { MulterModule } from '@nestjs/platform-express'
+import multer from 'multer'
+import path from 'path'
+import { MediaController } from 'src/routes/media/media.controller'
+import { generateRandomFilename } from 'src/shared/helpers'
+const UPLOAD_DIR = path.resolve('upload')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, UPLOAD_DIR)
+  },
+  filename: function (req, file, cb) {
+    const newFilename = generateRandomFilename(file.originalname)
+    cb(null, newFilename)
+  },
+})
+
+@Module({
+  imports: [
+    MulterModule.register({
+      storage,
+    }),
+  ],
+  controllers: [MediaController],
+})
+export class MediaModule {}
