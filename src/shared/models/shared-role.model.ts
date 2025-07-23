65b7bdf7943996e8ec5e4e53f39f00b0e5288731
hoapng <giahoa01giathieu@gmail.com>
2025-07-23 22:11:06 +0700

    CRUD User

--- src/shared/models/shared-role.model.ts
+++ src/shared/models/shared-role.model.ts
@@ -1,3 +1,4 @@
+import { PermissionSchema } from 'src/shared/models/shared-permission.model'
 import { z } from 'zod'
 
 export const RoleSchema = z.object({
@@ -12,4 +13,9 @@ export const RoleSchema = z.object({
   createdAt: z.date(),
   updatedAt: z.date(),
 })
+
+export const RolePermissionsSchema = RoleSchema.extend({
+  permissions: z.array(PermissionSchema),
+})
 export type RoleType = z.infer<typeof RoleSchema>
+export type RolePermissionsType = z.infer<typeof RolePermissionsSchema>
